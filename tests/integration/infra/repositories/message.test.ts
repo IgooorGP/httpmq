c61d95505c6198da3f8356d6209cc617c6f727a4
IgooorGP <igor.feq@gmail.com>
2020-10-25 18:33:21 -0300

    fix test regarding message type to be json

--- tests/integration/infra/repositories/message.test.ts
+++ tests/integration/infra/repositories/message.test.ts
@@ -1,12 +1,11 @@
 import { Connection } from "typeorm";
 import * as uuid from "uuid";
-import { Message } from "~/src/domain/models/message";
-import { Queue } from "~/src/domain/models/queue";
+import { Message } from "~/src/core/domain/models/message";
+import { Queue } from "~/src/core/domain/models/queue";
 import databaseConnection from "~/tests/support/db-connection";
 
-
 describe("Messaging repositories test suite", () => {
-  let dbConnection : Connection;
+  let dbConnection: Connection;
 
   beforeAll(async () => {
     dbConnection = await databaseConnection.create();
@@ -23,14 +22,14 @@ describe("Messaging repositories test suite", () => {
   test("should create a new message and queue using repositories", async () => {
     // arrange
     const messageRepository = dbConnection.getRepository(Message);
-    const queueRepository = dbConnection.getRepository(Queue);    
-    
+    const queueRepository = dbConnection.getRepository(Queue);
+
     const q = new Queue();
     const msg = new Message();
 
     q.name = `test_queue_${uuid.v4()}`;
     msg.headers = { "content-type": "text" };
-    msg.message = "hello world!";
+    msg.message = { message: "hello world!" };
 
     // act
     await messageRepository.save(msg);
@@ -38,7 +37,7 @@ describe("Messaging repositories test suite", () => {
 
     // assert
     const fetchedMessage = await messageRepository.findOneOrFail(msg.id);
-    expect(fetchedMessage.message).toBe("hello world!");
+    expect(fetchedMessage.message).toStrictEqual({ message: "hello world!" });
   });
 
   test("Should retrieve no messages from empty database", async () => {
